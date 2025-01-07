2544d65fde50a88b3f9210c272fefe7cd82830e2
unknown <khanr23@gmail.com>
2025-01-07 13:56:11 +0500

    Initial commit

---
+++ src/components/banner/LeftBanner.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaReact,
+  FaGithub,
+  FaWhatsapp,
+  FaWordpress,
+  FaShopify,
+} from "react-icons/fa";
+import { SiTailwindcss, SiFigma, SiNextdotjs, SiGmail } from "react-icons/si";
+
+const LeftBanner = () => {
+  const [text] = useTypewriter({
+    words: ["Expert Full-Stack Developer.", "Shopify & WordPress Specialist.", ""],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">Muhammad Rizwan</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+        As a full-stack developer with expertise in Shopify and WordPress, I craft custom, high-performance websites tailored to your business needs. I specialize in building scalable, user-friendly e-commerce solutions that drive conversions and deliver seamless experiences. With a focus on clean code and results-driven design, I help brands grow and succeed online
+        </p>
+      </div>
+      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <a href="https://github.com/58896" target="blank">
+              <span className="bannerIcon">
+                <FaGithub />
+              </span>
+            </a>
+            <a href="mailto:khanr2389@gmail.com" target="blank">
+              <span className="bannerIcon">
+                <SiGmail />
+              </span>
+            </a>
+            <a href="https://www.linkedin.com/in/rizwan-khan-2964b11a1/" target="blank">
+              <span className="bannerIcon">
+                <FaLinkedinIn />
+              </span>
+            </a>
+            <a href="https://wa.me/923495547791" target="blank">
+              <span className="bannerIcon">
+                <FaWhatsapp />
+              </span>
+            </a>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNextdotjs />
+            </span>
+            <span className="bannerIcon">
+              <FaWordpress />
+            </span>
+            <span className="bannerIcon">
+              <FaShopify/>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftBanner;
---
+++ src/components/resume/Experience.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { motion } from "framer-motion";
+import ResumeCard from "./ResumeCard";
+
+const Experience = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
+    >
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2024 - 2025</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Front End Developer"
+            subTitle="Firnas.Tech - (2024 - Present)"
+            result="Abbotabad"
+            des="Front-end developer with 1 year of experience building user-friendly and responsive web applications. Skilled in HTML, CSS, and JavaScript, with a strong understanding of UI/UX design principles. Passionate about creating beautiful and functional web experiences, and always eager to learn new technologies."
+          />
+          <ResumeCard
+            title="Internee"
+            subTitle="Internee - (6 Months)"
+            result="Abbotabad"
+            des="Front-end developer with experience in building user-friendly web applications. Previously interned at ExpandMore, where I gained experience in Shopify, WordPress, and front-end development."
+          />
+          <ResumeCard
+            title="Front-end Developer"
+            subTitle="UpWork- (Present)"
+            result="Pakistan"
+            des="Developed and maintained a variety of web applications using the MERN stack, including e-commerce platforms, social media apps. Proficient in React, Node.js, Express.js, and MongoDB."
+          />
+        </div>
+      </div>
+      <div>
+        {/* <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2001 - 2020</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Trainer Experience</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Gym Instructor"
+            subTitle="Rainbow Gym Center (2015 - 2020)"
+            result="DHAKA"
+            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+          />
+          <ResumeCard
+            title="Web Developer and Instructor"
+            subTitle="SuperKing College (2010 - 2014)"
+            result="CANADA"
+            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+          />
+          <ResumeCard
+            title="School Teacher"
+            subTitle="Kingstar Secondary School (2001 - 2010)"
+            result="NEVADA"
+            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+          />
+        </div> */}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Experience;
