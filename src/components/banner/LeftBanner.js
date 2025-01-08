f44d7b237a34cf438721c2f0c19a676f0869eda0
unknown <khanr23@gmail.com>
2025-01-08 15:45:19 +0500

    remove the a

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -14,7 +14,11 @@ import { SiNextdotjs, SiGmail } from "react-icons/si";
 
 const LeftBanner = () => {
   const [text] = useTypewriter({
-    words: ["Full-Stack Developer.", "Shopify Specialist.", "WordPress Specialist"],
+    words: [
+      "Full-Stack Developer.",
+      "Shopify Specialist.",
+      "WordPress Specialist",
+    ],
     loop: true,
     typeSpeed: 20,
     deleteSpeed: 10,
@@ -25,10 +29,11 @@ const LeftBanner = () => {
       <div className="flex flex-col gap-5">
         <h4 className="text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">Muhammad Rizwan</span>
+          Hi, I'm{" "}
+          <span className="text-designColor capitalize">Muhammad Rizwan</span>
         </h1>
         <h2 className="text-xl font-bold text-white sm:text-2px lg:text-4xl">
-           <span>{text}</span>
+          <span>{text}</span>
           <Cursor
             cursorBlinking="false"
             cursorStyle="|"
@@ -36,7 +41,12 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide mt-6 sm:mt-8 lg:mt-8">
-          As a full-stack developer with expertise in Shopify and WordPress, I craft custom, high-performance websites tailored to your business needs. I specialize in building scalable, user-friendly e-commerce solutions that drive conversions and deliver seamless experiences. With a focus on clean code and results-driven design, I help brands grow and succeed online.
+          As a full-stack developer with expertise in Shopify and WordPress, I
+          craft custom, high-performance websites tailored to your business
+          needs. I specialize in building scalable, user-friendly e-commerce
+          solutions that drive conversions and deliver seamless experiences.
+          With a focus on clean code and results-driven design, I help brands
+          grow and succeed online.
         </p>
       </div>
       <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
@@ -55,7 +65,10 @@ const LeftBanner = () => {
                 <SiGmail />
               </span>
             </a>
-            <a href="https://www.linkedin.com/in/rizwan-khan-2964b11a1/" target="blank">
+            <a
+              href="https://www.linkedin.com/in/rizwan-khan-2964b11a1/"
+              target="blank"
+            >
               <span className="bannerIcon">
                 <FaLinkedinIn />
               </span>
